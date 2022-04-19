fd44350d14906bc7f76f0554270c6cb9bfbaf54c
Sayreng-Hydra <Sayreng.hydra@gmail.com>
2022-04-19 15:40:17 +0200

    created multiple project pages

--- testing-site/pages/Electrolysis.tsx
+++ testing-site/pages/Electrolysis.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import ProjectImage from '../public/luca-bravo-XJXWbfSo2f0-unsplash.jpg';
+import ProjectImage from '../public/ramon-salinero-vEE00Hx5d0Q-unsplash.jpg';
 
 const DataAnalysis: NextPage = () => {
 	return (
 		<div>
-			<h1 className="text-xl text-indigo-600 mb-4 font-bold">Data Analysis app</h1>
+			<h1 className="text-xl text-indigo-600 mb-4 font-bold">Electrolysis monitoring program</h1>
 			<Image src={ProjectImage} alt="image" />
 			<div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat beatae ullam numquam pariatur nemo aliquid culpa sint eum. Nemo natus atque iure autem quidem magni at beatae nobis libero perspiciatis? Recusandae perferendis enim quo neque alias laudantium consequatur facere, accusantium, ut dolores impedit! Cupiditate eligendi dolorum at labore doloribus voluptates nisi nobis consectetur corrupti non vel, deleniti qui, libero ad?</div>
 			<div className="mt-6">
--- testing-site/pages/index.tsx
+++ testing-site/pages/index.tsx
@@ -4,19 +4,41 @@ import Profile from '../components/profile';
 import ProjectPreview, { ProjectPreviewInterface } from '../components/project_preview';
 
 const Home: NextPage = () => {
-	const projectPreviewData: ProjectPreviewInterface = {
+	const projectMetadataArray = [];
+
+	projectMetadataArray.push({
 		title: 'Data Analysis App',
 		desc: 'Processes data in Python',
 		linkURL: '/data_analysis',
 		image: '/luca-bravo-XJXWbfSo2f0-unsplash.jpg',
-	};
+	});
+
+	projectMetadataArray.push({
+		title: 'Data Pushtrough App',
+		desc: 'Sending data through api',
+		linkURL: '/data_pushtrough',
+		image: '/jj-ying-8bghKxNU1j0-unsplash.jpg',
+	});
+
+	projectMetadataArray.push({
+		title: 'Electrolysis App',
+		desc: 'Monitors conversion rate with python',
+		linkURL: '/Electrolysis',
+		image: '/ramon-salinero-vEE00Hx5d0Q-unsplash.jpg',
+	});
+
+	const projectPreviewElements = [];
+
+	for (let i = 0; i < projectMetadataArray.length; i++) {
+		const metaData = projectMetadataArray[i];
+		const element = <ProjectPreview {...metaData} key={i}></ProjectPreview>;
+		projectPreviewElements.push(element);
+	}
 
 	return (
 		<div>
 			<Profile></Profile>
-			<div className="mt-4">
-				<ProjectPreview {...projectPreviewData}></ProjectPreview>
-			</div>
+			<div className="mt-4 flex flex-col gap-2">{projectPreviewElements}</div>
 		</div>
 	);
 };
