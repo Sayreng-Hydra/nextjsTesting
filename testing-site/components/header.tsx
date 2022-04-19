48a53deacae51b9ec67399d2861cc92d6046a3b0
Sayreng-Hydra <Sayreng.hydra@gmail.com>
2022-04-19 14:12:46 +0200

    adjusted header component

--- testing-site/components/header.tsx
+++ testing-site/components/header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
 	return (
 		<div>
-			<div>Sayreng Hydra</div>
-			<div>Frontend developer</div>
+			<div className="text-2xl font-bold">Sayreng Hydra</div>
+			<div className="text-gray-500">Frontend developer</div>
 		</div>
 	);
 };
